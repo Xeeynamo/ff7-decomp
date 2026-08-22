a3e07dcba766403f7db7c930fb7a81b91bb7343e
halkun <37340407+joshuanwalker@users.noreply.github.com>
2026-08-22 15:00:37 -0500

    little fixes to externs

--- src/magic/lv5deth.c
+++ src/magic/lv5deth.c
@@ -1,9 +1,10 @@
 #include "common.h"
+#include "../battle/battle.h"
 
 typedef struct Lv5DeathData {
     s16 unk0;
     s16 unk2;
-    char unk4[8];
+    SVECTOR unk4;
     u16 unkC;
     s16 unkE;
     char pad10[0x10];
@@ -27,12 +28,20 @@ extern void MagicAnimationRegister(s32, s32, s32, void (*func)(void));
 extern void func_800D2980(void *, s32, s32, s32);
 extern s32 *func_800D29D4(s32, s32, s32, s32 *);
 extern void func_800D4368(void *, s16, s32);
+extern s32 func_800D4D90(s8*, s32, s32, s32);
+extern void func_800D5774(s16);
 
 /* LV5 Death functions. */
 extern void func_801B0000(void);
+extern void func_801B0074(void);
 extern void func_801B0310(void);
-extern void func_801B0414(void);
-void func_801B0508(s32 arg0, s32 arg1);
+extern void func_801B0414(s32 arg0);
+
+void func_801B01BC();
+
+
+
+extern func_801B0508(s32 arg0, s32 arg1);
 
 extern s32 D_801B05E0;
 extern s32 D_801D8E58;
@@ -123,12 +132,133 @@ void func_801B0074(void)
     }
 }
 
-
+#ifndef NON_MATCHINGS
 INCLUDE_ASM("asm/us/magic/nonmatchings/lv5deth", func_801B01BC);
+#else
+
+extern s16 D_8015169C;
+
+extern u8 D_80062D98;
+
+extern s8 D_801C0E48;
+extern volatile s8 D_801C0E49;
+extern volatile s8 D_801C0E4A;
+extern s16 D_801C0E4C;
+
+extern s32 D_801D8E50;
+extern s32 D_801D8E58;
+extern s32 g_cDb;
+
+extern void func_800D4368(void*, s16, s32);
+extern s32 func_800D4D90(s8*, s32, s32, s32);
+extern void func_800D5774(s16);
+
+void func_801B01BC(void) {
+    s16 temp_v1;
+    u8 var_v1;
+    s32 temp_a2;
+    s8* temp_s0;
+    s16* temp_unk2;
+    Lv5DeathData* temp_s1;
+
+    temp_s1 = &D_80162978[D_8015169C];
+
+    temp_unk2 = &temp_s1->unk2;
+
+    D_801C0E4C = *temp_unk2 & 7;
+
+    temp_v1 = *temp_unk2;
+
+    if (temp_v1 < 8) {
+        var_v1 = temp_v1 * 0x10;
+    } else if (temp_v1 >= 0x25) {
+        var_v1 = -0x80 - ((temp_v1 - 0x25) * 0x10);
+    } else {
+        var_v1 = 0x80;
+    }
+
+    temp_s0 = &D_801C0E48;
+
+    D_801C0E4A = var_v1;
+    D_801C0E49 = var_v1;
+    *(volatile s8*)temp_s0 = var_v1;
+
+    temp_a2 = temp_s1->unkC;
+
+    func_800D4368(
+        temp_s1->unk4,
+        (s16)temp_a2,
+        -((s32)(temp_a2 << 16) >> 18)
+    );
+
+    D_801D8E50 = func_800D4D90(
+        temp_s0 - 4,
+        g_cDb + 0x70,
+        0xC,
+        D_801D8E50
+    );
+
+    if (temp_s1->unk2 >= 0x2D) {
+        temp_s1->unk0 = -1;
+        D_801D8E58 -= 1;
+    }
+
+    if (D_80062D98 == 0) {
+        if (temp_s1->unk2 == 0x23) {
+            func_800D5774(temp_s1->unkE);
+        }
+
+        temp_s1->unk2++;
+    }
+}
+
+#endif
 
 INCLUDE_ASM("asm/us/magic/nonmatchings/lv5deth", func_801B0310);
 
+#ifndef NON_MATCHINGS
 INCLUDE_ASM("asm/us/magic/nonmatchings/lv5deth", func_801B0414);
+#else
+
+
+
+extern u8 D_8015190F[];
+
+void func_801B0414(s32 arg0)
+{
+    Lv5DeathData* temp_s0;
+    Lv5DeathData* temp_v0;
+    s32 temp_s1;
+    s8* temp_s3;
+
+    temp_s1 = arg0;
+
+    temp_s0 = &D_80162978[BattleEffectRegister(func_801B01BC)];
+
+    temp_s3 = (s8*)&temp_s0->unk4;
+
+    BattleGetPartPosition(
+        temp_s1,
+        D_8015190F[temp_s1 * 0xB9C],
+        temp_s3
+    );
+
+    temp_s0->unkC = 0x1CCC;
+    temp_s0->unkE = temp_s1;
+
+    temp_v0 = &D_80162978[BattleEffectRegister(func_801B0074)];
+
+    temp_v0->unk4 = temp_s0->unk4;
+
+    temp_v0->unkC = 0x13DC;
+
+    BattleCommandSend(
+        0x20,
+        BattlePositionToStereoPan(temp_s3),
+        0xAA
+    );
+}
+#endif
 
 
 void func_801B0508(s32 arg0, s32 arg1)
