ce3b7859f7b64db6a57e1dc66f5adb6704b4d4f8
halkun <37340407+joshuanwalker@users.noreply.github.com>
2026-08-23 22:58:42 -0500

    formatted

--- src/magic/lv5deth.c
+++ src/magic/lv5deth.c
@@ -13,13 +13,12 @@ typedef struct Lv5DeathData {
 /* Battle effect instance array. */
 extern Lv5DeathData D_80162978[];
 
-
 /* State used by the LV5 Death effect. */
 extern s16 D_80162080;
 extern s32 D_801C0E50;
 extern s32 D_801CCE50;
-extern s32 *D_801D8E50;
-extern s32 *D_801D8E54;
+extern s32* D_801D8E50;
+extern s32* D_801D8E54;
 extern s32 g_dbIndex;
 extern s32 D_801B05E0;
 extern s32 D_801D8E58;
@@ -37,19 +36,15 @@ extern s16 D_801C0E4C;
 extern s32 g_cDb;
 extern u8 D_8015190F[];
 
-
-
 /* Battle / magic engine functions. */
-extern void BattleGetPartPosition(s32 arg0, s32 arg1, void *arg2);
+extern void BattleGetPartPosition(s32 arg0, s32 arg1, void* arg2);
 extern s32 BattleEffectRegister(void (*func)(void));
 extern void MagicAnimationRegister(s32, s32, s32, void (*func)(void));
-extern void func_800D2980(void *, s32, s32, s32);
-extern s32 *func_800D29D4(s32, s32, s32, s32 *);
-extern void func_800D4368(SVECTOR *pos, s16 arg1, s32 arg2);
+extern void func_800D2980(void*, s32, s32, s32);
+extern s32* func_800D29D4(s32, s32, s32, s32*);
+extern void func_800D4368(SVECTOR* pos, s16 arg1, s32 arg2);
 extern s32 func_800D4D90(s8*, s32, s32, s32);
 extern void func_800D5774(s16);
-
-
 
 /* LV5 Death functions. */
 void func_801B0000(void);
@@ -59,11 +54,8 @@ void func_801B0310(void);
 void func_801B0414(s32 arg0);
 void func_801B0508(s32 arg0, s32 arg1);
 
-
-
-void func_801B0000(void)
-{
-    s32 *var_v1;
+void func_801B0000(void) {
+    s32* var_v1;
 
     var_v1 = &D_801CCE50;
 
@@ -78,60 +70,41 @@ void func_801B0000(void)
     }
 }
 
+void func_801B0054(void) { ((void (*)(void))func_801B0508)(); }
 
-
-
-void func_801B0054(void)
-{
-    ((void (*)(void))func_801B0508)();
-}
-
-
-
-void func_801B0074(void)
-{
+void func_801B0074(void) {
     char pad[0x34];
 
-    Lv5DeathData *temp_s0;
+    Lv5DeathData* temp_s0;
     s32 temp_a2;
     s32 temp_v1;
-    s32 *var_s1;
-    
+    s32* var_s1;
+
     temp_s0 = &D_80162978[D_8015169C];
 
     temp_a2 = temp_s0->unkC << 16;
 
-    func_800D4368(
-        &temp_s0->unk4,
-        temp_a2 >> 16,
-        -(temp_a2 >> 19)
-    );
+    func_800D4368(&temp_s0->unk4, temp_a2 >> 16, -(temp_a2 >> 19));
 
-    var_s1 = (s32 *)0x1F800000;
+    var_s1 = (s32*)0x1F800000;
 
     var_s1[0] = (s32)&D_801C0868;
     var_s1[1] = 0x88;
-    ((s16 *)var_s1)[4] = 0;
-    ((s16 *)var_s1)[5] = 0x800;
-    ((s16 *)var_s1)[6] = 0;
-    ((s16 *)var_s1)[7] = 0;
-
+    ((s16*)var_s1)[4] = 0;
+    ((s16*)var_s1)[5] = 0x800;
+    ((s16*)var_s1)[6] = 0;
+    ((s16*)var_s1)[7] = 0;
 
     temp_v1 = temp_s0->unk2;
     if (temp_v1 < 8) {
         temp_v1 <<= 8;
-        ((s16 *)var_s1)[5] = 0x1000 - temp_v1;
+        ((s16*)var_s1)[5] = 0x1000 - temp_v1;
     } else if (temp_v1 >= 0x25) {
-        ((s16 *)var_s1)[5] = (temp_v1 << 8) - 0x1D00;
+        ((s16*)var_s1)[5] = (temp_v1 << 8) - 0x1D00;
     }
     SetFarColor(0, 0, 0);
 
-    D_801D8E50 = func_800D29D4(
-        (s32)var_s1,
-        g_cDb + 0x70,
-        0xC,
-        D_801D8E50
-    );
+    D_801D8E50 = func_800D29D4((s32)var_s1, g_cDb + 0x70, 0xC, D_801D8E50);
 
     if (temp_s0->unk2 >= 0x2D) {
         temp_s0->unk0 = -1;
@@ -141,7 +114,6 @@ void func_801B0074(void)
         temp_s0->unk2++;
     }
 }
-
 
 void func_801B01BC(void) {
     s16 temp_v1;
@@ -173,20 +145,11 @@ void func_801B01BC(void) {
     D_801C0E49 = var_v1;
     *(volatile s8*)temp_s0 = var_v1;
 
-  temp_a2 = temp_s1->unkC;
+    temp_a2 = temp_s1->unkC;
 
-func_800D4368(
-    &temp_s1->unk4,
-    (s16)temp_a2,
-    -((s32)(temp_a2 << 16) >> 18)
-);
+    func_800D4368(&temp_s1->unk4, (s16)temp_a2, -((s32)(temp_a2 << 16) >> 18));
 
-    D_801D8E50 = func_800D4D90(
-        temp_s0 - 4,
-        g_cDb + 0x70,
-        0xC,
-        D_801D8E50
-    );
+    D_801D8E50 = func_800D4D90(temp_s0 - 4, g_cDb + 0x70, 0xC, D_801D8E50);
 
     if (temp_s1->unk2 >= 0x2D) {
         temp_s1->unk0 = -1;
@@ -202,14 +165,11 @@ func_800D4368(
     }
 }
 
-
-
-
 void func_801B0310(void) {
     s16 temp_v0;
     s16 var_v1;
     s16 temp_v1;
-    Lv5DeathData *temp_a0;
+    Lv5DeathData* temp_a0;
 
     temp_a0 = &D_80162978[D_8015169C];
     temp_v0 = temp_a0->unk2;
@@ -219,7 +179,7 @@ void func_801B0310(void) {
         D_800F5B71 = 0;
         D_800F5B70 = 0;
 
-        __asm__ volatile ("" : : : "memory");
+        __asm__ volatile("" : : : "memory");
 
         temp_v1 = temp_a0->unk2;
         var_v1 = temp_v1 * 0x140;
@@ -245,30 +205,23 @@ void func_801B0310(void) {
     }
 }
 
-
 #ifndef NON_MATCHINGS
 INCLUDE_ASM("asm/us/magic/nonmatchings/lv5deth", func_801B0414);
 #else
 
-
-void func_801B0414(s32 arg0)
-{
-    Lv5DeathData *temp_s0;
-    Lv5DeathData *temp_v0;
+void func_801B0414(s32 arg0) {
+    Lv5DeathData* temp_s0;
+    Lv5DeathData* temp_v0;
     s32 temp_s1;
-    s8 *temp_s3;
+    s8* temp_s3;
 
     temp_s1 = arg0;
 
     temp_s0 = &D_80162978[BattleEffectRegister(func_801B01BC)];
 
-    temp_s3 = (s8 *)&temp_s0->unk4;
+    temp_s3 = (s8*)&temp_s0->unk4;
 
-    BattleGetPartPosition(
-        temp_s1,
-        D_8015190F[temp_s1 * 0xB9C],
-        temp_s3
-    );
+    BattleGetPartPosition(temp_s1, D_8015190F[temp_s1 * 0xB9C], temp_s3);
 
     temp_s0->unkC = 0x1CCC;
     temp_s0->unkE = temp_s1;
@@ -279,20 +232,14 @@ void func_801B0414(s32 arg0)
 
     temp_v0->unkC = 0x13DC;
 
-    BattleCommandSend(
-        0x20,
-        BattlePositionToStereoPan(temp_s3),
-        0xAA
-    );
+    BattleCommandSend(0x20, BattlePositionToStereoPan(temp_s3), 0xAA);
 }
 #endif
 
-
-void func_801B0508(s32 arg0, s32 arg1)
-{
+void func_801B0508(s32 arg0, s32 arg1) {
     s32 var_a0;
     s32 var_v1;
-    Lv5DeathData *var_v0;
+    Lv5DeathData* var_v0;
 
     D_801D8E54 = &D_80162978[BattleEffectRegister(func_801B0000)];
 
