6445abfd691ea3385c1de9430a9fafe09ee22e2b
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2026-09-11 17:48:10 +0100

    Remove overlapping, false symbols (#159)

--- src/battle/battle.h
+++ src/battle/battle.h
@@ -268,7 +268,9 @@ typedef struct {
     /* 0x1E72 */ u16 D_800F7DC6;
     /* 0x1E84 */ u16 battleType;
     /* 0x1E84 */ u16 D_800F7DCA;
-    /* 0x1E88 */ u8 unk1E88[0x12];
+    /* 0x1E88 */ u8 unk1E88[6];
+    /* 0x1E8E */ u16 reviveMask;
+    /* 0x1E90 */ u8 unk1E90[0xA];
     /* 0x1E9A */ s16 D_800F7DDE;
 } Unk800F5F44; // size:0x1E9C
 
@@ -510,14 +512,10 @@ typedef struct {
 
 extern BattleWork g_BattleWork;
 extern Unk800F5F44 D_800F5F44;
-extern u8 D_800F6934[0x40][8];
-extern s8 D_800F6936[0x40][8];
-extern u8 D_800F6B34[10][8];
 extern s8 D_800F6B86[2][8];
 extern u16 D_800F7DE8;
 extern u8 D_800F83A8;
 extern BattleState g_BattleState;
-extern s8 D_800F90B4[][0x240];
 extern Unk800BB75C D_800FA63C;
 extern DB* g_cDb;
 extern short g_BattleEffectCursor;
