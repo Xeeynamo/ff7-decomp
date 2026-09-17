5043b12382c1781b62c63bd3700fbaa1d7010cb4
Halkun <37340407+halkuncode@users.noreply.github.com>
2026-09-17 14:05:35 -0500

    More structs, globals, and functions identified in BatInitMain() (#171)
    Went through BatInitMain() and found and named almost all struct,
    globals, and functions. Any missing I followed and named after
    confirming function, identified and Added MANY new struct members to
    many core battle structs that helped fill out many functions in battle.c
    proper.
    
    I'm not a pleading kinda guy, but can you _**please**_ merge this
    first... 🥺

--- src/main/144D8.c
+++ src/main/144D8.c
@@ -22,7 +22,7 @@ static void* D_80048DD4[] = {
 };
 
 // obtain file sector from a YamadaFile
-s32 func_800144D8(s32 file_no) { return D_80048D84[file_no].loc; }
+s32 SystemGetFileLBA(s32 file_no) { return D_80048D84[file_no].loc; }
 
 static void func_800144F0(s32 file_no) { func_80033DAC(file_no, 0); }
 
@@ -35,7 +35,7 @@ void func_80014578(s32 file_no, void* dst, void (*cb)(void)) {
     SystemLoadFileBySector(D_80048D84[file_no].loc, D_80048D84[file_no].len, dst, cb);
 }
 
-void func_800145BC(void (*cb)(void)) {
+void SystemCdWaitCallback(void (*cb)(void)) {
     while (SystemCdromReadChain()) {
         if (cb) {
             cb();
@@ -49,13 +49,13 @@ static void func_80014608(void) {}
 void func_80014610(void) {
     u8 buf[2048];
     SystemLoadFileBySector(LBA_INIT_YAMADA, sizeof(buf), (u_long*)&buf, NULL);
-    func_800145BC(0);
+    SystemCdWaitCallback(0);
     SysMemCopy32(D_80048D84, &buf, sizeof(Yamada) * YAMADA_FILE_NUM);
 }
 
 static void func_80014658(s32 file_no, void (*cb)(void)) {
     func_80014578(file_no, (void*)0x801B0000, 0);
-    func_800145BC(0);
+    SystemCdWaitCallback(0);
     SysGzipBinDecompress((GzHeader*)0x801B0000, (u8*)0x800A0000);
     cb();
 }
@@ -65,7 +65,7 @@ void func_800146A4(void) {
     while (var_s0) {
         switch (D_8009C560) {
         case 4:
-            func_800145BC(0);
+            SystemCdWaitCallback(0);
             func_80014658(BATTLE_BROM, D_800A00CC);
             break;
         case 2:
