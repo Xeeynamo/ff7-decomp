f3aa4d8581cfcda62d34da07388d2389376d4e18
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-09-19 16:11:20 +0100

    field: FieldCameraAssign

--- src/field/field_camera.c
+++ src/field/field_camera.c
@@ -11,6 +11,12 @@ INCLUDE_ASM("asm/us/field/nonmatchings/field_camera", HandleKawaiDataInModel);
 // Possable Debug routine. Ran at beginning of every main field loop. (FPS?)
 void DebugRunEveryLoop(void) {}
 
-INCLUDE_ASM("asm/us/field/nonmatchings/field_camera", FieldCameraAssign);
+void FieldCameraAssign(void) {
+    if (D_80114488 == 0 || D_8009AC2D == 1) {
+        D_80071E40 = *D_80083578;
+    } else {
+        D_80071E40 = D_80083270;
+    }
+}
 
 INCLUDE_ASM("asm/us/field/nonmatchings/field_camera", FieldUpdateMovieStream);
